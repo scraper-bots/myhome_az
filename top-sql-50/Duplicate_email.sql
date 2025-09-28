ea1d5640f244082df6e8a928750760c6f56b8efa
ismat-samadov <ismetsemedov@gmail.com>
2025-09-28 22:58:38 +0400

    init

--- top-sql-50/Duplicate_email.sql
+++
@@ -1,4 +0,0 @@
-/* Write your PL/SQL query statement below */
-select email  from (
-  select email,count(1) say from person group by email having count(1)>1
-) 
--- top-sql-50/Employees Earning More Than Their Managers.sql
+++
@@ -1,7 +0,0 @@
-/* Write your PL/SQL query statement below */
-select mng.name  manager, mng.salary manager_salary ,
-       emp.name  employee,emp.salary employee_salary
- from employee emp 
- inner join employee mng 
- on emp.managerId=mng.id
- where emp.salary>mng.salary;
